37e0783fd5eea686ce555f830c6b2ce81813f62f
mr.samani <mr.samani@outlook.com>
2024-07-24 13:13:44 +0330

    remove ngx-drop-kit-component

--- projects/ngx-drag-drop-kit/src/public-api.ts
+++ projects/ngx-drag-drop-kit/src/public-api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of ngx-drag-drop-kit
  */
 
-export * from './lib/ngx-drag-drop-kit.service';
-export * from './lib/ngx-drag-drop-kit.component';
 
 export * from './lib/directives/ngx-draggable.directive';
 export * from './lib/directives/ngx-resizable.directive';
