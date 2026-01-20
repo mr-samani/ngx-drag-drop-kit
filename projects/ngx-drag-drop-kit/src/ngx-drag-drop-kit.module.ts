5ae1acb3735875c5958456064f387719aaafd7bf
mr.samani <mr.samani1368@gmail.com>
2026-01-20 10:23:29 +0330

    update peerDependencies

--- projects/ngx-drag-drop-kit/src/ngx-drag-drop-kit.module.ts
+++ projects/ngx-drag-drop-kit/src/ngx-drag-drop-kit.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { NgxDropListDirective } from './lib/directives/ngx-drop-list.directive';
-import { NgxPlaceholderDirective } from './lib/directives/ngx-place-holder.directive';
 import { NgxDraggableDirective } from './lib/directives/ngx-draggable.directive';
 import { NgxResizableDirective } from './lib/directives/ngx-resizable.directive';
 const standAlones = [
